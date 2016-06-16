bee401c3c201dbc53cc4902c76b1cbbec29f0ac0
Sara Gabriela Díaz Oporto <sara.diaz.oporto@gmail.com>
2016-06-16 13:00:48 -0500

    ya!

--- Ejercicio 10 HOTEL CAMA ARENA/Problema/main.js
+++ Ejercicio 10 HOTEL CAMA ARENA/Problema/main.js
@@ -1,1 +1,5 @@
-//Escribe aquí tu código
+alert("¡Bienvenido!");
+var noches=parseFloat(prompt("¿De cuántas noches será la estancia?"));
+var costo=parseFloat(prompt("¿Cuánto cuesta la habitación por noche?"));
+var total=noches*costo;
+alert(noches+" noches de estancia costarán "+total+" soles");
--- Ejercicio 9 LA LLAMADA TELEFÓNICA/Problema/main.js
+++ Ejercicio 9 LA LLAMADA TELEFÓNICA/Problema/main.js
@@ -1,1 +1,5 @@
-//Escribe aquí tu código
+alert("¡Bienvenido!");
+var min=parseFloat(prompt("¿Cuántos minutos durará la llamada?"));
+var costo=parseFloat(prompt("¿Cuál es el costo de un minuto de llamada?"));
+var resultado=min*costo;
+alert("Una llamada de "+min+" minutos costará "+resultado+" soles");
